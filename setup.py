325677fc9cb70ff9ea45a577663980623af70a56
Colin Gould <colin.gould15@gmail.com>
2025-09-14 01:39:47 -0400

    bug, new version 1.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (this_directory / "README.md").read_text(encoding='utf-8')
 
 setup(
     name="mantis-web-crawler",
-    version="1.0.0",
+    version="1.1.0",
     description="Web accessibility and UI bug detection tool",
     long_description=long_description,
     long_description_content_type="text/markdown",
